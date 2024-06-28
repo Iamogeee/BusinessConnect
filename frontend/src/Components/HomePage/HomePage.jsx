0b80f1017a71fbffbcc04d7e883272fe8c970d29
Ogenna Obiora <ogenna@meta.com>
2024-06-28 11:12:57 -0700

    Implemented user authentication using jwt

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import MainHeader from "../MainHeader/MainHeader";
 import "./HomePage.css";
 
 const HomePage = () => {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const fetchHomeData = async () => {
+      const token = localStorage.getItem("token");
+      const response = await fetch("/home", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+      } else {
+        setError("Failed to fetch data");
+      }
+    };
+
+    fetchHomeData();
+  }, []);
+
   return (
     <div className="home-page">
       <MainHeader />
