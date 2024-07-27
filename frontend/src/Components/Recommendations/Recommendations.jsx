c992187b77cb707043a67f40f4201d99af730530
Ogenna Obiora <ogenna@meta.com>
2024-07-26 20:59:59 -0700

    updated recs page

--- frontend/src/Components/Recommendations/Recommendations.jsx
+++ frontend/src/Components/Recommendations/Recommendations.jsx
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import MainHeader from "../MainHeader/MainHeader";
 import BusinessList from "../BusinessList/BusinessList";
 
 const Recommendations = () => {
+  const [selectedCategory, setSelectedCategory] = useState("");
   const user = JSON.parse(localStorage.getItem("user"));
 
   return (
-    <BusinessList
-      title="Recommended"
-      fetchBusinessesUrl={`http://localhost:3000/recommendations/${user.id}`}
-      userId={user.id}
-    />
+    <div className="home-page">
+      <MainHeader onCategoryChange={setSelectedCategory} userId={user?.id} />
+      <main>
+        <h2>Recommended Businesses</h2>
+        {user && (
+          <BusinessList
+            apiEndpoint={`http://localhost:3000/recommendations/${user.id}`}
+            selectedCategory={selectedCategory}
+          />
+        )}
+      </main>
+    </div>
   );
 };
 
